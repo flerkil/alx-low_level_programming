381dfc64e9c524750701261534f06dd302dd0fd7
flerkil <flerkil99@gmail.com>
2023-06-16 05:20:04 -0700

    tset

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,7 +10,6 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
 	if (n == 0)
 {
 	printf("%d is zero\n", n);
@@ -23,9 +22,5 @@ int main(void)
 {
 	printf("%d is negative\n", n);
 }
-
-
-
-
 	return (0);
 }
