2f3bde7812a716264547d55b966be784c89d934d
flerkil <flerkil99@gmail.com>
2023-06-20 05:06:33 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_alphabet - print all alphabet in lowercase
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+char n[26] = "abcdefghijklmnopqrstuvwxyz";
+
+int i;
+
+for (i = 0; i < 26; i++)
+{
+_putchar(n[i]);
+print_alphabet();
+_putchar("\n")
+}
+}
